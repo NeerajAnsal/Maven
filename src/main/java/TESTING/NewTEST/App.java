01a48127910e5416300577c300b099911ab72ed7
neeraj <neerajansal86@gmail.com>
2018-09-22 12:27:22 +0530

    Build

---
+++ src/main/java/TESTING/NewTEST/App.java
@@ -0,0 +1,13 @@
+package TESTING.NewTEST;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
